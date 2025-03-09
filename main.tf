bacfa0801ca787649c995c833be7536d6062738e
Chethan S <chethans@Mac.lan>
2025-03-09 10:01:00 +0530

    IAM Role

--- main.tf
+++ main.tf
@@ -1,25 +1,49 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_basic_role"
 
-  required_version = ">= 1.2.0"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
 }
 
-provider "aws" {
-    region     = var.AWS_REGION
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_ACCESS_KEY
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "s3_read_only"
+  description = "Allow EC2 to read from S3"
+  
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
 }
