692c43a5c88cebf26d2d91c1b54496074614df9a
Chethan S <chethans@Mac.lan>
2025-03-09 10:17:34 +0530

    IAM Role

--- main.tf
+++ main.tf
@@ -1,45 +1,26 @@
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_basic_role"
-
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
   }
-  EOF
+
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_iam_policy" "s3_read_policy" {
-  name        = "s3_read_only"
-  description = "Allow EC2 to read from S3"
-  
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": "*"
-      }
-    ]
-  }
-  EOF
+provider "aws" {
+    region     = var.AWS_REGION
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_read_policy.arn
-}
+# Fetch AWS Account ID dynamically
+data "aws_caller_identity" "current" {}
 
+# Replace Account ID in template.json dynamically
+resource "aws_cloudformation_stack" "ec2_stack" {
+  name          = "FileTransfer"
+  template_body = templatefile("template.json", { account_id = data.aws_caller_identity.current.account_id })
+
+}
